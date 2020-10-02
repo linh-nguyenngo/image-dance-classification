92f6645b252d107cb4ec5da93b8e15eeb8c8f480
minh dao <minhdao6@gmail.com>
2020-10-02 15:13:19 +0700

    Add comments to explains the code

--- mnist/mnist_cnn.py
+++ mnist/mnist_cnn.py
@@ -6,25 +6,31 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Dropout
 
+# Load dataset
 train_imgs = mnist.train_images()
 train_labels = mnist.train_labels()
 
 test_imgs = mnist.test_images()
 test_labels = mnist.test_labels()
 
+# Normalize the dataset
 train_imgs = (train_imgs/255) - .5
 test_imgs = (test_imgs/255) - .5
 
+# Expand dimension to get the correct shapes for training
 train_imgs = np.expand_dims(train_imgs, axis=3)
 test_imgs = np.expand_dims(test_imgs, axis=3)
 
+# Check shapes of the dataset
 # print(train_imgs.shape)
 # print(test_imgs.shape)
 
+# Define variables to use in the model
 num_filters = 8
 filter_size = 3
 pool_size = 2
 
+# Create model
 model = Sequential([
   Conv2D(num_filters, filter_size, input_shape=(28, 28, 1)),
   Conv2D(num_filters, filter_size),
@@ -39,6 +45,7 @@ model.compile(
   metrics=['accuracy'],
 )
 
+# Start training
 model.fit(
   train_imgs,
   to_categorical(train_labels),
--- mnist/mnist_cnn_from_images.py
+++ mnist/mnist_cnn_from_images.py
@@ -5,15 +5,18 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 
+# Load dataset
 test_data_set = keras.preprocessing.image_dataset_from_directory('./testing', batch_size=64, image_size=(28, 28))
 train_data_set = keras.preprocessing.image_dataset_from_directory('./training', batch_size=64, image_size=(28, 28))
 
+# Define variables to use in the model
 scaling = Rescaling(scale=1./255)
 num_filters =32
 filter_size = 3
 pool_size = 2
 num_classes = 10
 
+# Define the model
 model = Sequential([
   scaling,
   Conv2D(num_filters, filter_size),
@@ -33,6 +36,7 @@ model.compile(
   metrics=['accuracy'],
 )
 
+# Start training
 model.fit(
   train_data_set,
   epochs=3,
